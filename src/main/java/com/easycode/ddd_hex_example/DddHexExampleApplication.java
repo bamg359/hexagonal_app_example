a2e8d09c705184a0f04cfc662158a6f6b3b03f51
Ber Mira <bamg359@gmail.com>
2025-03-23 08:38:47 -0500

    App with hex and DDD concepts

---
+++ src/main/java/com/easycode/ddd_hex_example/DddHexExampleApplication.java
@@ -0,0 +1,13 @@
+package com.easycode.ddd_hex_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DddHexExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DddHexExampleApplication.class, args);
+	}
+
+}
